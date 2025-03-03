f8500ea28195bdef7a2607af7f29b042249e51ce
Henry Geovani Orellana Vasquez <U20210307@univo.edu.sv>
2025-03-02 23:01:53 -0600

    Agregar archivos de proyecto.

---
+++ WebApplication2/Migrations/20250302171020_HMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class HMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NombreCompleto",
+                table: "Alumnos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NombreCompleto",
+                table: "Alumnos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
